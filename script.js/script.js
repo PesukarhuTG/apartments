354591c27a6fa7f88caf47d7b1d2131ef1af12e8
Tatiana <ganzhinovat@yandex.ru>
2021-12-12 19:00:19 +0300

    Add a floors counter + highlight an active floor

---
+++ script.js/script.js
@@ -0,0 +1,64 @@
+const homeImageContainer = document.querySelector('.home-image');
+const floorImages = document.querySelectorAll('path');
+const buttonUp = document.querySelector('.counter-up');
+const buttonDown = document.querySelector('.counter-down');
+const counter = document.querySelector('.counter');
+
+let currentFloor = 2;
+let allFloorNumber = floorImages.length; //17
+
+//hide an inactive floor image
+const hideFloorImg = (num, direction) => {
+    floorImages[num - 2].removeAttribute('style'); //delete opacity=1
+
+    if (direction === 'up') {
+        if (num === 2) {
+            floorImages[allFloorNumber - 1].removeAttribute('style');
+        }
+    } else if (direction === 'down') {
+        if (num === allFloorNumber + 1) {
+            floorImages[0].removeAttribute('style');
+        }
+    }
+};
+
+function showCurrentData(num = 2) {
+    counter.textContent = (currentFloor < 10) ? `0${currentFloor}` : currentFloor;
+    floorImages[num - 2].style.opacity = 1; //show an active floor image
+}
+
+// choose the floor by mouse clicking
+homeImageContainer.addEventListener('mouseover', e => {
+    hideFloorImg(currentFloor);
+
+    if (e.target.closest('path')) {
+        currentFloor = e.target.dataset.floor;
+        showCurrentData(currentFloor);
+    }
+})
+
+buttonUp.addEventListener('click', () => {
+    if (currentFloor < allFloorNumber + 1) {
+        hideFloorImg(currentFloor, 'up'); //example, hide 2
+        ++currentFloor; //3
+        showCurrentData(currentFloor); //show 3
+    } else {
+        currentFloor = 2;
+        hideFloorImg(currentFloor, 'up'); //hide 18
+        showCurrentData(currentFloor); // show 2
+    }
+})
+
+buttonDown.addEventListener('click', () => {
+    if (currentFloor > 2) {
+        hideFloorImg(currentFloor, 'down'); //example, hide 5
+        --currentFloor; //4
+        showCurrentData(currentFloor); //show 4
+    } else {
+        currentFloor = allFloorNumber + 1;
+        hideFloorImg(currentFloor, 'down'); //hide 2
+        showCurrentData(currentFloor); //show 18
+    }
+})
+
+showCurrentData();
